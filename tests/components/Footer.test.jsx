a0a3298e9841920843996f0e21a12773ec3930ad
Michał Siwiec <siwiec.michal724@gmail.com>
2022-07-30 07:39:53 +0200

    fix test helper name

--- tests/components/Footer.test.jsx
+++ tests/components/Footer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../../src/components/footer/Index.jsx';
-import renderWithProviders from '../components/helpers/render_with_state.jsx';
+import renderWithProviders from '../components/helpers/render_with_providers.jsx';
 
 test('render Footer component with success', () => {
   renderWithProviders(<Footer />);
