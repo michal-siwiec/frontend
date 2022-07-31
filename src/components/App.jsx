1c55bd70554e42726da19b51e8ed42648dd07034
Michał Siwiec <siwiec.michal724@gmail.com>
2022-07-31 18:47:09 +0200

    add better styles for advertising box

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import '../styles/style.scss';
 import TopBar from './topBar/Index.jsx';
-import AdvertisingBox from './advertisingBox/Index.jsx';
+import AdvertisingBox from './advertisingBox/advertisingBox.jsx';
 import Main from './main/Index.jsx';
 import Newsletter from './newsletter/Index.jsx';
 import Footer from './footer/Index.jsx';
