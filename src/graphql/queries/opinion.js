513073344b5d4a74fb3acf03424f00ffebbec6f3
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-08 20:01:53 +0200

    add pagination for opinions

--- src/graphql/queries/opinion.js
+++ src/graphql/queries/opinion.js
@@ -1,18 +1,21 @@
 import { gql } from '@apollo/client';
 
-export const GET_OPINIONS = gql(`
-  query {
-    opinions {
-      content
-      mark
-      updatedAt
-      user {
-        email
-        avatars {
-          main
-          storagePath
+export const GET_OPINIONS = gql`
+  query($input: OpinionFiltrationInput) {
+    opinionsDetails: opinions(input: $input) {
+      opinions {
+        content
+        mark
+        updatedAt
+        user {
+          email
+          avatars {
+            main
+            storagePath
+          }
         }
       }
+      allOpinionsQuantity
     }
   }
-`);
+`;
--- src/views/main/opinions/Opinions.jsx
+++ src/views/main/opinions/Opinions.jsx
@@ -5,30 +5,37 @@ import { GET_OPINIONS } from 'graphql/queries/opinion.js';
 import useIsLogged from 'hooks/useIsLogged.jsx';
 import AddingOpinionSuccessModal from 'components/modals/AddingOpinionSuccessModal.jsx';
 import AddingOpinionErrorModal from 'components/modals/AddingOpinionErrorModal.jsx';
+import Pagination from 'components/Pagination.jsx';
 import OpinionsList from './OpinionsList.jsx';
 import EmptyOpinionsList from './EmptyOpinionsList.jsx';
 import AddOpinionForm from './AddOpinionForm.jsx';
 
 const Opinions = () => {
   const blockName = 'opinions';
+  const quantityPerPage = 2;
   const isLogged = useIsLogged();
+  const [activePage, setActivePage] = useState(0);
   const [isOpinionAdded, setIsOpinionAdded] = useState(false);
   const [isAddedOpinionError, setIsAddedOpinionError] = useState(false);
+  const textareaRef = useRef(null);
   const {
     loading,
     error,
     data,
     refetch
-  } = useQuery(GET_OPINIONS);
-  const textareaRef = useRef(null);
+  } = useQuery(
+    GET_OPINIONS,
+    { variables: { input: { pagination: { page: activePage, quantityPerPage } } } }
+  );
 
   // Tutaj trzeba zrobić dwa modale
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>error</h1>;
 
-  const { opinions } = data;
+  const { opinionsDetails: { allOpinionsQuantity, opinions } } = data;
   const opinionsEmpty = isEmpty(opinions);
 
+  const handlePaginationOnChange = (pageNumber) => setActivePage(pageNumber - 1);
   const closeAddedOpinionModal = () => setIsOpinionAdded(false);
   const closeAddedOpinionErrorModal = () => setIsAddedOpinionError(false);
 
@@ -40,7 +47,7 @@ const Opinions = () => {
           : <OpinionsList opinions={opinions} />
       }
       {
-        !isLogged && (
+        isLogged && (
           <AddOpinionForm
             setIsOpinionAdded={setIsOpinionAdded}
             setIsAddedOpinionError={setIsAddedOpinionError}
@@ -56,6 +63,12 @@ const Opinions = () => {
       <AddingOpinionErrorModal
         isOpen={isAddedOpinionError}
         handleOnClose={closeAddedOpinionErrorModal}
+      />
+      <Pagination
+        activePage={activePage}
+        onChange={handlePaginationOnChange}
+        itemsQuantity={allOpinionsQuantity}
+        quantityPerPage={quantityPerPage}
       />
     </div>
   );
