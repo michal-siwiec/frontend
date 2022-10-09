2366188dcbcc9e83a0c60ac6a0400e0ca8037529
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-09 12:55:51 +0200

    add pagination for products

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,15 +7,26 @@ const Pagination = ({
   onChange,
   itemsQuantity,
   quantityPerPage
-}) => (
-  <ReactJSPagination
-    activePage={activePage}
-    itemsCountPerPage={quantityPerPage}
-    totalItemsCount={itemsQuantity}
-    pageRangeDisplayed={5}
-    onChange={onChange}
-  />
-);
+}) => {
+  const blockName = 'pagination';
+
+  return (
+    <div className={blockName}>
+      <ReactJSPagination
+        activePage={activePage + 1}
+        itemsCountPerPage={quantityPerPage}
+        totalItemsCount={itemsQuantity}
+        pageRangeDisplayed={5}
+        onChange={onChange}
+        innerClass={`${blockName}__list`}
+        itemClass={`${blockName}__list-item`}
+        activeClass={`${blockName}__list-item--active`}
+        activeLinkClass={`${blockName}__item-link--active`}
+        hideNavigation
+      />
+    </div>
+  );
+};
 
 Pagination.propTypes = exact({
   activePage: number.isRequired,
