d7d2a53823966b8612764480c491d9e23d3bdb9f
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-15 11:21:55 +0200

    fix problem with mobility in topbar

--- src/hooks/useIsMobile.jsx
+++ src/hooks/useIsMobile.jsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from 'react';
-import { debounce } from 'lodash';
 import widthBreakpoints from 'data/widthBreakpoints.js';
 
 const useIsMobile = (mobileBreakPoint_ = widthBreakpoints.md) => {
   const mobileBreakPoint = mobileBreakPoint_;
-  const timeAfterCallFunction = 250;
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < mobileBreakPoint);
 
   const setMobility = () => {
     setIsMobile(window.innerWidth < mobileBreakPoint);
   };
 
   useEffect(() => {
-    window.addEventListener('resize', debounce(setMobility, timeAfterCallFunction));
+    window.addEventListener('resize', setMobility);
 
     return () => window.removeEventListener('resize', setMobility);
   }, []);
