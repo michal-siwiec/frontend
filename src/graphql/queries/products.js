15c8cad99605db8117c6572ec27dc40d347aa83a
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-22 22:20:06 +0200

    remove unused code

--- src/graphql/queries/products.js
+++ src/graphql/queries/products.js
@@ -1,14 +1,15 @@
 import { gql } from '@apollo/client';
-import { productFields } from '../fragments/productFields.js';
 
 export const GET_PRODUCTS = gql`
-  ${productFields}
-
   query ($input: ProductInput) {
     productsDetails: products(input: $input) {
       quantity,
       products {
-        ...productFields
+        id
+        name
+        price
+        availableQuantity
+        picturePath
       }
     }
   }
