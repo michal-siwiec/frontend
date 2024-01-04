e285604330c587b195f26229e6d52ecfbdb5274e
Michal Siwiec <michalsiwiec@192.168.1.104>
2024-01-04 22:03:46 +0100

    rename views to pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './styles/style.scss';
 import useSetAuthInitialState from 'hooks/useSetAuthInitialState.jsx';
 import TopBar from 'layouts/topBar/TopBar.jsx';
 import AdvertisingBox from 'layouts/AdvertisingBox.jsx';
-import AppRouter from 'views/AppRouter.jsx';
+import AppRouter from 'pages/AppRouter.jsx';
 import Newsletter from 'layouts/Newsletter.jsx';
 import Footer from 'layouts/Footer.jsx';
 
