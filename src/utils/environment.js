e7afa6695572a6588140e9ceac231a52dd37b065
Michal Siwiec <michalsiwiec@MBP-Tadeusz.lan>
2024-01-13 21:44:16 +0100

    remove hardcoded bucket name

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -7,6 +7,7 @@ const API_WS_ROOT = process.env.API_WS_ROOT;
 const AWS_REGION = process.env.AWS_REGION;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_BUCKET = process.env.AWS_BUCKET;
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
 
@@ -17,5 +18,6 @@ export {
   AWS_REGION,
   AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY,
+  AWS_BUCKET,
   isProductionEnv
 };
