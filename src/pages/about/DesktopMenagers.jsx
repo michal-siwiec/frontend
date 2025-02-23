d48eda5bad6ac14c53bb74f465e93a39586982c5
Michał Siwiec <michal.siwiec@vouch.us>
2025-02-23 12:44:04 +0100

    change name

--- src/pages/about/DesktopMenagers.jsx
+++ src/pages/about/DesktopMenagers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import useFetchUrl from 'hooks/useFetchUrl.jsx';
 import ShadowedContainer from 'components/containers/ShadowedContainer.jsx';
-import { appearingInSequence } from 'data/animations.js';
+import { APPEARING_IN_SEQUENCE } from 'data/animations.js';
 import { managers, mappedDirections } from 'data/about.js';
 
 const DesktopMenagers = () => {
@@ -24,10 +24,10 @@ const DesktopMenagers = () => {
         }, index) => (
           <ShadowedContainer
             animationAttributes={{
-              variants: appearingInSequence,
+              variants: APPEARING_IN_SEQUENCE,
               custom: index,
-              initial: appearingInSequence.hidden,
-              animate: appearingInSequence.visible
+              initial: APPEARING_IN_SEQUENCE.hidden,
+              animate: APPEARING_IN_SEQUENCE.visible
             }}
             key={uuidv4()}
           >
