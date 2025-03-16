e906ebe4052ae91d0dbc05940dafc57414454a51
Michał Siwiec <michal.siwiec@vouch.us>
2025-03-16 15:01:58 +0100

    Add unit tests for Tooltip.jsx

---
+++ tests/unit/components/Tooltip.rtl.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Tooltip from 'components/Tooltip';
+
+describe('Tooltip Component', () => {
+  it('renders without crashing', () => {
+    render(
+      <Tooltip open={true} headerText="Test Header">
+        <button>Hover me</button>
+      </Tooltip>
+    );
+  });
+
+  it('displays content when open', () => {
+    render(
+      <Tooltip open={true} headerText="Test Header" secondaryText="Test Secondary">
+        <button>Hover me</button>
+      </Tooltip>
+    );
+
+    expect(screen.getByText('Test Header')).toBeInTheDocument();
+    expect(screen.getByText('Test Secondary')).toBeInTheDocument();
+    expect(screen.getByText('Hover me')).toBeInTheDocument();
+  });
+
+  it('does not show content when is close', () => {
+    render(
+      <Tooltip open={false} headerText="Test Header" secondaryText="Test Secondary">
+        <button>Hover me</button>
+      </Tooltip>
+    );
+
+    const children = screen.queryByText('Hover me');
+
+    expect(screen.queryByText('Test Header')).not.toBeInTheDocument();
+    expect(screen.queryByText('Test Secondary')).not.toBeInTheDocument();
+    // MUI generate this inside DOM with 0 size so it's invisible
+    expect(children.getBoundingClientRect().width).toBe(0);
+    expect(children.getBoundingClientRect().height).toBe(0);
+  });
+});
--- tests/unit/components/VawyDots.rtl.test.js
+++ tests/unit/components/VawyDots.rtl.test.js
@@ -3,11 +3,11 @@ import { render } from '@testing-library/react';
 import VawyDots from 'components/VawyDots.jsx';
 
 describe('VavyDots Component', () => {
-  test('renders without crashing', () => {
+  it('renders without crashing', () => {
     render(<VawyDots dotsCount={3} />);
   });
 
-  test('renders the correct number of dots', () => {
+  it('renders the correct number of dots', () => {
     const { container } = render(<VawyDots dotsCount={5} />);
 
     expect(container.getElementsByClassName('dot').length).toBe(5);
