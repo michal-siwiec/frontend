14bc503e9d5ab15c0e2e3b2180516388522314e2
Michał Siwiec <michal.siwiec@vouch.us>
2025-03-31 23:45:12 +0200

    fix component name

--- tests/unit/components/modals/ErrorModal.rtl.test.js
+++ tests/unit/components/modals/ErrorModal.rtl.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, screen, fireEvent, within } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ErrorModal from 'components/modals/ErrorModal';
 
-describe('ErrorModal component', () => {
+describe('ErrorModal', () => {
   const mockClose = jest.fn();
 
   const setup = (props = {}) => {
