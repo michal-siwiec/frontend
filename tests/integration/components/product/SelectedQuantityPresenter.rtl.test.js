6e2af4d14487ecd7259a54572113a8e24d7f0db8
Michał Siwiec <michal.siwiec@vouch.us>
2025-04-19 19:37:28 +0200

    fix test name

--- tests/integration/components/product/SelectedQuantityPresenter.rtl.test.js
+++ tests/integration/components/product/SelectedQuantityPresenter.rtl.test.js
@@ -4,7 +4,7 @@ import renderWithProviders from '../../helpers/renderWithProviders.js'; // TODO 
 import SelectedQuantityPresenter from 'components/product/SelectedQuantityPresenter.jsx';
 
 describe('SelectedQuantityPresenter', () => {
-  it('renders the quantity from the store for the given product id', () => {
+  it('renders component successfully', () => {
     const preloadedState = { basket: { addedProducts: [{ id: '088fc480-ce29-4d10-852a-971d60a01e59', quantity: 3 }] }};
     renderWithProviders(<SelectedQuantityPresenter id="088fc480-ce29-4d10-852a-971d60a01e59" />, { preloadedState });
 
