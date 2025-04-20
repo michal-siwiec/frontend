e9b3e9b73a52e9bb2fb9aed7e1d7a8736425908f
Michał Siwiec <michal.siwiec@vouch.us>
2025-04-20 12:50:08 +0200

    Add unit tests for 'useIsLogged'

---
+++ tests/unit/helpers/createProvidersWrapper.js
@@ -0,0 +1,18 @@
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from 'redux_/rootReducer.js';
+import { ApolloProvider } from '@apollo/client';
+import Client from 'graphql/Client.js';
+
+// TODO: Update readme to know how to turn on linters and various tests
+
+const createProvidersWrapper = (preloadedState = {}) => {
+  const store = configureStore({ reducer: rootReducer, preloadedState });
+  return ({ children }) => (
+    <ApolloProvider client={Client}>
+      <Provider store={store}>{children}</Provider>
+    </ApolloProvider>
+  );
+};
+
+export default createProvidersWrapper;
