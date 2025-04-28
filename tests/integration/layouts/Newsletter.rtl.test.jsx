1a8aa23e73108521f6b3f9066a0a93358c85a5fa
Michał Siwiec <michal.siwiec@vouch.us>
2025-04-28 19:23:17 +0200

    Add draft state for 'Newsletter'

---
+++ tests/integration/layouts/Newsletter.rtl.test.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import renderWithProviders from 'tests/integration/helpers/renderWithProviders.jsx';
+import Newsletter from 'layouts/Newsletter.jsx';
+import { USER_PERSONAL_DETAILS, IS_USER_SAVED_TO_NEWSLETTER } from 'graphql/queries/user';
+
+const renderComponent = ({ mocks, preloadedState }) => (
+  renderWithProviders(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <Newsletter />
+    </MockedProvider>,
+    { preloadedState }
+  )
+)
+
+describe('Newsletter', () => {
+  it('does not render component when user is logged and already saved to newsletter', async () => {
+    const loggedUserId = '0c1069c7-8e77-4749-bc4b-e308c6679d1c';
+    const preloadedState = { user: { loggedUserId } };
+
+    const mocks = [
+      {
+        request: {
+          query: USER_PERSONAL_DETAILS,
+          variables: { userId: loggedUserId },
+        },
+        result: {
+          data: {
+            user: {
+              __typename: 'UserObject',
+              id: loggedUserId,
+              name: 'Michal',
+              surname: 'Siwiec',
+              email: 'siwiec.michal724@gmail.com',
+              phoneNumber: '724131140',
+              city: 'Gliwice',
+              postalCode: '44-100',
+              street: 'Zwycięstwa'
+            },
+          },
+        },
+      },
+      {
+        request: {
+          query: IS_USER_SAVED_TO_NEWSLETTER,
+          variables: { userId: loggedUserId },
+        },
+        result: {
+          data: {
+            user: {
+              __typename: 'UserObject',
+              id: loggedUserId,
+              savedToNewsletter: true,
+              email: 'siwiec.michal724@gmail.com'
+            },
+          },
+        },
+      }
+    ];
+
+    renderComponent({ mocks, preloadedState });
+
+    await waitFor(() => {
+      expect(screen.queryByPlaceholderText('Imię')).not.toBeInTheDocument();
+      expect(screen.queryByPlaceholderText('Nazwisko')).not.toBeInTheDocument();
+      expect(screen.queryByPlaceholderText('Adres email')).not.toBeInTheDocument();
+      expect(screen.queryByText('Zapisz')).not.toBeInTheDocument();
+    });
+  });
+});
