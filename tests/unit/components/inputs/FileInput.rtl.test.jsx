055e1db3cee03d4ee5f35e7657eda0da8dbbaf5f
Michał Siwiec <michal.siwiec@vouch.us>
2025-05-21 12:57:11 +0200

    fix RTL

--- tests/unit/components/inputs/FileInput.rtl.test.jsx
+++ tests/unit/components/inputs/FileInput.rtl.test.jsx
@@ -7,18 +7,18 @@ describe('FileInput component', () => {
   const mockRef = React.createRef();
 
   it('renders component properly', () => {
-    const { container } = render(<FileInput onChange={mockOnChange} innerRef={mockRef} dataCy="file-input" />);
+    const { container } = render(<FileInput onChange={mockOnChange} innerRef={mockRef} dataTestID="file-input" />);
 
-    const input = container.querySelector('[data-cy="file-input"]');
+    const input = container.querySelector('[data-testid="file-input"]');
     expect(input).toBeInTheDocument();
     expect(input).toHaveAttribute('type', 'file');
   });
 
   it('calls onChange handler when file is selected', () => {
-    const { container } = render(<FileInput onChange={mockOnChange} innerRef={mockRef} dataCy="file-input" />);
+    const { container } = render(<FileInput onChange={mockOnChange} innerRef={mockRef} dataTestID="file-input" />);
 
     const file = new File(['hello'], 'hello.png', { type: 'image/png' });
-    const input = container.querySelector('[data-cy="file-input"]');
+    const input = container.querySelector('[data-testid="file-input"]');
     fireEvent.change(input, { target: { files: [file] } });
 
     expect(mockOnChange).toHaveBeenCalled();
@@ -26,7 +26,7 @@ describe('FileInput component', () => {
 
   it('shows validation error if provided', () => {
     const errorText = 'This field is required.';
-    render(<FileInput onChange={mockOnChange} innerRef={mockRef} dataCy="file-input" validationError={errorText} />);
+    render(<FileInput onChange={mockOnChange} innerRef={mockRef} dataTestID="file-input" validationError={errorText} />);
 
     expect(screen.getByText(errorText)).toBeInTheDocument();
     expect(screen.getByText(errorText)).toHaveClass('input__error');
