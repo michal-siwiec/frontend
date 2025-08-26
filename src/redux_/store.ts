c5c5ab11b910a3045151f5d821ab003f75f862ae
Michał Siwiec <michalsiwiec@192.168.1.110>
2025-08-26 08:19:35 +0200

    fix

--- src/redux_/store.ts
+++ src/redux_/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
-import rootReducer from './rootReducer.js';
+import rootReducer from './rootReducer';
 import { isProductionEnv } from 'utils/environment';
 
 export const store = configureStore({
