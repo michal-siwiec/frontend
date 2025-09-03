f11e94101f3bc502486a739381a1deb2376d0ed7
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2025-09-03 21:48:55 +0200

    Remove package v6

--- config/webpack.config.default.js
+++ config/webpack.config.default.js
@@ -27,20 +27,6 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                quality: 15
-              }
-            }
-          }
-        ]
-      },
-      {
         test: /\.(js|jsx|ts|tsx)$/i,
         loader: 'babel-loader',
         exclude: /node_modules/,
