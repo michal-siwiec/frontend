524cfa2c8d2ee3b6ca84d3df59479660989115f7
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2025-09-03 16:59:42 +0200

    fix lint

--- src/data/uiElements.ts
+++ src/data/uiElements.ts
@@ -2,10 +2,8 @@
 export const STEP_LABELS = ['Dane odbiorcy', 'Sposób dostawy', 'Metoda płatności', 'Podsumowanie'];
 
 // Footer
-/* eslint-disable max-len */
 export const SHOP_RULES_TEXT = 'Regulamin sklepu internetowego to zbiór zasad i norm, regulujących procesy związane z dokonywanymi na stronie sklepu transakcjami. Również w regulaminie znajdziemy informację określające zasady komunikacji sklepu z klientem oraz odwrotnej możliwości. Często są tu zawarte również informacje o przesyłkach i płatnościach.';
 export const PRIVACY_POLICY_TEXT = 'Polityka prywatności – dokument umieszczany na witrynie internetowej w celu poinformowania użytkowników o tym, jakie dane osobowe są o nich zbierane i jak będą wykorzystywane.';
-/* eslint-enable max-len */
 
 // Contact data
 export const SHOP_MAIL = 'siwiec.michal724@gmail.com';
